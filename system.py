f315feaeec76ccc62ce4a9a0ce4e53948ab38893
apkqiu <apkqiu@qq.com>
2026-08-12 17:17:14 +0800

    feat: add env parameter to execute functions

--- system.py
+++ system.py
@@ -4,6 +4,7 @@ from collections.abc import Iterable
 
 from .logger import getLogger
 
+
 def parse_cmd(cmdline):
     cmd = []
     for i in cmdline:
@@ -14,14 +15,16 @@ def parse_cmd(cmdline):
         else:
             cmd.append(str(i))
     return cmd
-def execute(*cmdline, check=True, log=True):
+
+
+def execute(*cmdline, env:dict[str, str]|None=None, check=True, log=True):
     logger = getLogger("ShellExecute", not log)
     out_logger = getLogger("ShellExecute_tty", not log)
     cmd = parse_cmd(cmdline)
     logger.info(f"Executing \033[33m'{"' '".join(cmd)}'\033[0m")
     r1, w1 = os.pipe2(os.O_NONBLOCK)
     r2, w2 = os.pipe2(os.O_NONBLOCK)
-    p = subprocess.Popen(cmd, stdout=w1, stderr=w2)
+    p = subprocess.Popen(cmd, stdout=w1, stderr=w2, env=env)
     stdout = open(r1, "rb")
     stderr = open(r2, "rb")
     prev_outbuf = b""
@@ -74,13 +77,16 @@ def execute(*cmdline, check=True, log=True):
         raise ChildProcessError(f"Process exited with exit code {code}")
     logger.info(f"Process exited with exit code {code}")
 
-def execute_capture(*cmdline, check=True, log=True):
+
+def execute_capture(*cmdline, env:dict[str, str]|None=None, check=True, log=True):
     logger = getLogger("ShellExecute", not log)
     out_logger = getLogger("ShellExecute_tty", not log)
     cmd = parse_cmd(cmdline)
-    logger.info(f"Executing \033[33m'{"' '".join(cmd)}'\033[0m - Output has been captured.")
+    logger.info(
+        f"Executing \033[33m'{"' '".join(cmd)}'\033[0m - Output has been captured."
+    )
     try:
-        out = subprocess.check_output(cmd,encoding="utf-8")
+        out = subprocess.check_output(cmd, encoding="utf-8", env=env)
         for line in out.splitlines():
             out_logger.info(line)
     except subprocess.CalledProcessError as e:
